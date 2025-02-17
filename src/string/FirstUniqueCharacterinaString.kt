5776cb9ab2768c68c7fcd1cec55780807c4657fb
Javad <josmithth2828@gmail.com>
2025-02-17 23:25:10 +0330

    update the "387. First Unique Character in a String" Leetcode problem

--- src/string/FirstUniqueCharacterinaString.kt
+++ src/string/FirstUniqueCharacterinaString.kt
@@ -2,11 +2,11 @@ package string
 
 fun firstUniqChar(s: String): Int {
 
-    val map = s.withIndex().groupBy({ it.value }, { it.index })
-    for (item in s) {
-        val key = map[item]
-        if (key?.size == 1) {
-            return key.first()
+    val map = s.groupingBy { it }.eachCount()
+
+    for ((index, item) in s.withIndex()) {
+        if (map.getValue(item) == 1) {
+            return index
         }
     }
     return -1
