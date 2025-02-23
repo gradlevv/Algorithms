26e3570543a2de9d2f4588ca7acd0eeef6fbe328
Javad <josmithth2828@gmail.com>
2025-02-23 23:52:45 +0330

    solve the "206. Reverse Linked List" Leetcode problem

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,7 @@
 import array.intersection
 import array.maxProfit
+import linked_list.ListNode
+import linked_list.reverseList
 import string.firstUniqChar
 import string.lengthOfLongestSubstring
 import string.validPalindrome
@@ -12,5 +14,18 @@ fun main() {
     val num2 = intArrayOf(2, 2)
     val s = charArrayOf('h', 'e', 'z', 'l', 'o')
     val string = "loveleetcode"
-    println(intersection(num1, num2))
+//    println(intersection(num1, num2))
+
+    val node1 = ListNode(1)
+    val node2 = ListNode(2)
+    val node3 = ListNode(3)
+    val node4 = ListNode(4)
+    val node5 = ListNode(5)
+
+    node1.next = node2
+    node2.next = node3
+    node3.next = node4
+    node4.next = node5
+
+    println(reverseList(node1)?.value)
 }
---
+++ src/linked_list/ReverseLinkedList.kt
@@ -0,0 +1,21 @@
+package linked_list
+
+fun reverseList(head: ListNode?): ListNode? {
+    if (head == null) return null
+
+    var prev: ListNode? = null
+    var current = head
+    var next: ListNode?
+    while (current != null) {
+        next = current.next
+        current.next = prev
+        prev = current
+        current = next
+
+    }
+    return prev
+}
+
+class ListNode(var value: Int) {
+    var next: ListNode? = null
+}
