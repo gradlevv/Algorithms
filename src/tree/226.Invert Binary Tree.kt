89ef78e810450e45ea9d6abd9090dccba923cc16
Javad <j.sadatii@gmail.com>
2025-03-22 21:56:34 +0330

    update the "226. Invert Binary Tree" Leetcode problem

--- src/tree/226.Invert Binary Tree.kt
+++ src/tree/226.Invert Binary Tree.kt
@@ -1,5 +1,8 @@
 package tree
 
+import java.util.LinkedList
+import java.util.Queue
+
 fun invertTree(root: TreeNode?): TreeNode? {
 
     if (root == null) return null
@@ -8,6 +11,31 @@ fun invertTree(root: TreeNode?): TreeNode? {
 
     invertTree(root.left)
     invertTree(root.right)
+
+    return root
+}
+
+fun invertTree2(root: TreeNode?): TreeNode? {
+
+    if (root == null) return null
+
+    val queue: Queue<TreeNode> = LinkedList()
+    queue.add(root)
+
+    while (queue.isNotEmpty()) {
+
+        val current = queue.remove()
+        current.right = current.left.also { current.left = current.right }
+
+        if (current.left != null) {
+            queue.add(current.left)
+        }
+        if (current.right != null) {
+            queue.add(current.right)
+        }
+
+    }
+
 
     return root
 }
