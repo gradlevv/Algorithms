0064f481c70a6fdb9ed1054425baf983654b7cfa
Javad <j.sadatii@gmail.com>
2025-03-25 22:32:56 +0330

    update the "112. Path Sum" Leetcode problem

--- src/tree/112.Path Sum.kt
+++ src/tree/112.Path Sum.kt
@@ -1,5 +1,9 @@
 package tree
 
+import java.util.LinkedList
+import java.util.Queue
+
+//recursive
 fun hasPathSum(root: TreeNode?, targetSum: Int): Boolean {
 
     if (root == null) return false
@@ -7,4 +11,27 @@ fun hasPathSum(root: TreeNode?, targetSum: Int): Boolean {
     val remainingSum = targetSum - (root.`val`)
 
     return hasPathSum(root.left, remainingSum) || hasPathSum(root.right, remainingSum)
+}
+
+//iterative
+fun hasPathSum2(root: TreeNode?, targetSum: Int): Boolean {
+
+    if (root == null) return false
+
+    val queue: Queue<Pair<TreeNode, Int>> = LinkedList()
+    queue.add(root to targetSum)
+
+    while (queue.isNotEmpty()) {
+
+        val (node, target) = queue.poll()
+        if (node.`val` == target && (node.left == null && node.right == null)) return true
+
+        val remainingSum = target - node.`val`
+        if (node.left != null)
+            queue.add(node.left to remainingSum)
+        if (node.right != null)
+            queue.add(node.right to remainingSum)
+    }
+
+    return false
 }
