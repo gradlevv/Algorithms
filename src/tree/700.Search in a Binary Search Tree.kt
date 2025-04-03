98397def0e60d31be26402de84eaee123105860e
Javad <j.sadatii@gmail.com>
2025-04-03 22:23:22 +0330

    update the "700. Search in a Binary Search Tree" Leetcode problem

--- src/tree/700.Search in a Binary Search Tree.kt
+++ src/tree/700.Search in a Binary Search Tree.kt
@@ -8,3 +8,19 @@ fun searchBST(root: TreeNode?, `val`: Int): TreeNode? {
         else -> searchBST(root.left, `val`)
     }
 }
+
+fun searchBST2(root: TreeNode?, `val`: Int): TreeNode? {
+    if (root == null) return null
+    if (root.`val` == `val`) return root
+
+    var current = root
+    while (current != null) {
+        current = when {
+            current.`val` == `val` -> return current
+            current.`val` < `val` -> current.right
+            else -> current.left
+        }
+    }
+
+    return null
+}
