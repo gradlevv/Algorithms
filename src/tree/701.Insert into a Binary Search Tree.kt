68610c2b8411866bcddc009e8f875d00ec9724a9
Javad <j.sadatii@gmail.com>
2025-04-04 20:54:06 +0330

    solve the "200. Number of Islands" Leetcode problem

--- src/tree/701.Insert into a Binary Search Tree.kt
+++ src/tree/701.Insert into a Binary Search Tree.kt
@@ -8,3 +8,32 @@ fun insertIntoBST(root: TreeNode?, `val`: Int): TreeNode? {
     }
     return root
 }
+
+fun insertIntoBST2(root: TreeNode?, `val`: Int): TreeNode? {
+
+    if (root == null) return TreeNode(`val`)
+
+    var current = root
+    var parent: TreeNode = root
+    while (current != null) {
+        parent = current
+        current = when {
+            current.`val` < `val` -> {
+                current.right
+            }
+
+            else -> {
+                current.left
+            }
+        }
+
+    }
+
+    if (parent.`val` < `val`) {
+        parent.right = TreeNode(`val`)
+    } else {
+        parent.left = TreeNode(`val`)
+    }
+
+    return root
+}
