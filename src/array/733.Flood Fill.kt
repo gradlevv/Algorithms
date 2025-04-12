c9e6341e8a5285ae3fb1010008d033de59079c62
Javad <j.sadatii@gmail.com>
2025-04-12 23:39:15 +0330

    update the "733. Flood Fill" Leetcode problem

--- src/array/733.Flood Fill.kt
+++ src/array/733.Flood Fill.kt
@@ -1,5 +1,8 @@
 package array
 
+import java.util.LinkedList
+import java.util.Queue
+
 fun floodFill(image: Array<IntArray>, sr: Int, sc: Int, color: Int): Array<IntArray> {
 
     if (sr !in image.indices || sc !in image[0].indices) return image
@@ -16,6 +19,36 @@ fun floodFill(image: Array<IntArray>, sr: Int, sc: Int, color: Int): Array<IntAr
                 floodFill(image, newX, newY, color)
             }
         }
+    }
+
+    return image
+}
+
+fun floodFillBFS(image: Array<IntArray>, sr: Int, sc: Int, color: Int): Array<IntArray> {
+
+    val originalColor = image[sr][sc]
+    if (originalColor == color) return image
+
+    val queue: Queue<Pair<Int, Int>> = LinkedList()
+    queue.add(sr to sc)
+    image[sr][sc] = color
+
+    val directions = listOf(-1 to 0, 1 to 0, 0 to -1, 0 to 1)
+
+    while (queue.isNotEmpty()) {
+
+        val (x, y) = queue.remove()
+        for ((dx, dy) in directions) {
+            val newX = x + dx
+            val newY = y + dy
+            if (newX in image.indices && newY in image[0].indices) {
+                if (image[newX][newY] == originalColor) {
+                    image[newX][newY] = color
+                    queue.add(newX to newY)
+                }
+            }
+        }
+
     }
 
     return image
